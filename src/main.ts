5b2bc959c8488c230ddd7819c935945f6574fc4d
aevastylelol <aevastylelol@gmail.com>
2018-03-06 10:27:45 +0000

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ class GameWindow {
             .with_color(Color.from_u8(34, 153, 84, 1.0))
             .with_color(Color.from_u8(231, 76, 60, 1.0))
             .with_color(Color.from_u8(108, 52, 131, 1.0))
-            .with_swap_transition(350, 'linear')
+            .with_swap_transition(250, 'linear')
             .with_hide_transition(350, 'linear')
             .with_shift_transition(350, 'linear')
             .with_select_transition(250, 'linear');
@@ -41,76 +41,71 @@ class GameWindow {
 
 const main = () => {
     const game_window = new GameWindow();
+    const ui = UI.instance_of();
+    const background_sound = BackgroundSound.instance_of();
+    const sound_fx = SoundFX.instance_of();
 
-    UI.instance_of().play_mods.buttons.play_on_time.add_event_callback('click', () => {
-        const game = game_window.play_on_time;
+    ui.play_mods.buttons.play_on_time.add_event_callback('click', () => {
+        const game = game_window.play_on_time.init_game();
+        const game_context = game.context();
 
-        game.init_game();
-        game.set_time_callback(time_left => console.log(time_left));
-        game.start_game();
-        BackgroundSound.instance_of().main_theme.reset().play();
+        background_sound.main_theme.reset().play();
 
         game.set_time_callback(time_left => {
-            const res = time_left.toString();
-            UI.instance_of().info_panel.time_element.element.textContent = res.length >= 2 ? res : `0${res}`;
-            UI.instance_of().info_panel.score_element.element.textContent = game.context().score().toString();
-
-            if (time_left === 20) {
-                UI.instance_of().info_panel.add_time_is_low();
-            }
+            ui.info_panel.set_time(time_left);
+            ui.info_panel.set_score(game_context.score());
         });
 
         const handler = (ev: MouseEvent) => {
-            if (game.context().is_selected_block()) {
-                game.context().selected_swap_with(ev);
+            if (game_context.is_selected_block()) {
+                game_context.selected_swap_with(ev);
             } else {
-                game.context().select(ev);
+                game_context.select(ev);
             }
         };
 
         game.set_game_over_callback(() => {
-            game_window.canvas
-                .clear();
+            background_sound.main_theme.stop();
 
-            UI.instance_of().info_panel.remove_time_is_low();
-            UI.instance_of().main_page.buttons.continue.element.off();
-            UI.instance_of().game_over.element.on();
-            UI.instance_of().game_over.score_element.element.textContent = game.context().score().toString();
+            game_window.canvas.clear();
 
-            BackgroundSound.instance_of().main_theme.stop();
+            ui.main_page.buttons.continue.element.off();
+            ui.game_over.element.on();
+            ui.game_over.set_score(game_context.score());
 
             game_window.canvas.event_target().removeEventListener('click', handler);
         });
 
         game_window.canvas.event_target().addEventListener('click', handler);
+
+        game.start_game();
     });
 
-    UI.instance_of().settings.background_sound.element.off();
+    ui.settings.background_sound.element.off();
 
     const background_sound_callback = (ev?: MouseEvent) => {
-        if (UI.instance_of().settings.background_sound.element.is_off()) {
-            BackgroundSound.instance_of().main_theme.off();
+        if (ui.settings.background_sound.element.is_off()) {
+            background_sound.main_theme.off();
         } else {
-            BackgroundSound.instance_of().main_theme.on();
+            background_sound.main_theme.on();
         }
     };
 
     background_sound_callback();
-    UI.instance_of().settings.background_sound.button.add_event_callback('click', background_sound_callback);
+    ui.settings.background_sound.button.add_event_callback('click', background_sound_callback);
 
     const sound_fx_callback = (ev?: MouseEvent) => {
-        if (UI.instance_of().settings.sound_fx.element.is_off()) {
-            SoundFX.instance_of().hide.off();
-            SoundFX.instance_of().shift.off();
+        if (ui.settings.sound_fx.element.is_off()) {
+            sound_fx.hide.off();
+            sound_fx.shift.off();
         } else {
-            SoundFX.instance_of().hide.on();
-            SoundFX.instance_of().shift.on();
+            sound_fx.hide.on();
+            sound_fx.shift.on();
         }
     };
 
     sound_fx_callback();
-    UI.instance_of().settings.sound_fx.button.add_event_callback('click', sound_fx_callback)
-
+    ui.settings.sound_fx.button.add_event_callback('click', sound_fx_callback)
 };
 
 window.addEventListener('load', main);
