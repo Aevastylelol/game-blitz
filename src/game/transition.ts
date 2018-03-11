b16094194b3b24b66409a3b01b8c9c40972f2dee
aevastylelol <aevastylelol@gmail.com>
2018-03-11 09:35:25 +0000

    add accelerated transition fn

--- src/game/transition.ts
+++ src/game/transition.ts
@@ -1,4 +1,4 @@
-import { SequenceCallback } from './../core/sequencer';
+import { SequenceCallback } from './../core/mod';
 
 type TransitionFunction = (delta: number, step_callbackfn: (step: number) => void) => SequenceCallback;
 
@@ -20,6 +20,37 @@ class Transition {
 
             if (time_passed < this.duration) {
                 now = time_passed / this.duration * delta;
+
+                callbackfn(now - last);
+
+                last = now;
+
+                window.requestAnimationFrame(loop)
+            } else {
+                now = delta;
+
+                callbackfn(now - last);
+
+                done();
+            }
+        };
+
+        window.requestAnimationFrame(loop);
+    }
+
+    public readonly accelerated = (delta: number, callbackfn: (step: number) => void): SequenceCallback => (done: () => void = () => { }) => {
+        const a = delta / (this.duration ** 2);
+
+        const start_time = Date.now();
+
+        let last = 0;
+        let now = 0;
+
+        const loop = () => {
+            const time_passed = Date.now() - start_time;
+
+            if (time_passed < this.duration) {
+                now = a * (time_passed ** 2);
 
                 callbackfn(now - last);
 
